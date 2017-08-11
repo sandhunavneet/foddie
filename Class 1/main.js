ba4f23d0a5cb7f9da9ba4466909018d927e6be6a
sandhunavneet <sandhu1998gnit@gmail.com>
2017-08-11 10:00:05 +0530

    this is foodieapp

---
+++ Class 1/main.js
@@ -0,0 +1,64 @@
+var foodieApp = angular.module('foodieApp',['ngRoute']);
+foodieApp.config(function ($routeProvider) {
+	$routeProvider
+	.when('/',{
+		templateUrl: 'pages/login.html',
+		controller: 'loginController'
+	})
+	.when('/home',{
+		templateUrl: 'pages/main.html',
+		controller: 'mainController'
+	})
+})
+  $scope.restaurants = [{
+	name: 'Farzi Cafe',
+	address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+	location: 'Connaught Place',
+	category: 'Casual Dining, Bar',
+	vote: '4.2',
+	cuisines: 'Modern Indian',
+	cost: '2200',
+	hours: '12 Noon to 1 AM (Mon-Sun)',
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+
+},
+
+{
+name: 'Farzi Cafe',
+address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+location: 'Connaught Place',
+category: 'Casual Dining, Bar',
+vote: '4.2',
+cuisines: 'Modern Indian',
+cost: '2200',
+hours: '12 Noon to 1 AM (Mon-Sun)',
+image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},
+{
+name: 'Farzi Cafe',
+address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+location: 'Connaught Place',
+category: 'Casual Dining, Bar',
+vote: '4.2',
+cuisines: 'Modern Indian',
+cost: '2200',
+hours: '12 Noon to 1 AM (Mon-Sun)',
+image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+},
+{
+name: 'Farzi Cafe',
+address: '38/39, Level 1, Block E , Inner Circle, Connaught Place',
+location: 'Connaught Place',
+category: 'Casual Dining, Bar',
+vote: '4.2',
+cuisines: 'Modern Indian',
+cost: '2200',
+hours: '12 Noon to 1 AM (Mon-Sun)',
+image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+}]
+})
+
+
+
+foodieApp.controller('loginController',function($scope) {
+})
